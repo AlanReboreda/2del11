3870a8b7a819aa4902346bce98ad7139ddc3d995
AlanReboreda <92063942+AlanReboreda@users.noreply.github.com>
2021-11-02 00:47:09 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+let numero: number[] = new Array(7);
+
+let indice: number = 0;
+numero[0] = 20;
+numero[1] = 14;
+numero[2] = 5;
+numero[3] = 24;
+numero[4] = 5;
+numero[5] = 64;
+numero[6] = 54;
+numero[7] = 1;
+
+while (indice < 7) {
+  console.log("el numero en la pocision es", indice, "es", numero[indice]);
+  indice++;
+}
